0e1dc4f577c5c16c3b3c0d5e7513f192f668bbf9
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-10-05 09:27:13 -0400

    :)

--- MethodMadness/src/LLiProject1.java
+++ MethodMadness/src/LLiProject1.java
@@ -1,4 +1,13 @@
+//Author: Lily Li
+//Date Created: 10/5/2017
+// Date Completed:
 
 public class LLiProject1 {
-
+	public static boolean isPalindrome(String word) {
+		char[] characters = word.toCharArray();
+	    for(int i = 0; i<= characters.length; i++) {
+	    	System.out.print(characters[i]); 
+	    }
+	    return true; 
+	}
 }
