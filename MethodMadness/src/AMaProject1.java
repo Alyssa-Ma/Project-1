4c34786fca9ae342d4070824efb17826d36c8c48
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-10-10 09:21:34 -0400

    i don't like this

--- MethodMadness/src/AMaProject1.java
+++ MethodMadness/src/AMaProject1.java
@@ -8,15 +8,10 @@ public class AMaProject1
 		dateformatedit = dateformat.substring( 0, 3);
 		String datenewformat = "09 - 10 - 2017";
 	}
-	
-	public static void date()
-	{
-		
-	}
 	public static void multiplicationtable(int r)
 	{
 		int count = 0;
-		for(int n = 0; count <= 0; n++)
+		for(int n = 0; count >= 0; n++)
 		{
 			System.out.println(n * r);
 		}
