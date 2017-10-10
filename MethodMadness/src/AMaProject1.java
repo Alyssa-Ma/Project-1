4442a19e566e86290b9fea6c94ff86ec445546f6
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-10-10 09:05:13 -0400

    help

--- MethodMadness/src/AMaProject1.java
+++ MethodMadness/src/AMaProject1.java
@@ -1,4 +1,25 @@
 
-public class AMaProject1 {
-
+public class AMaProject1 
+{
+	public static String dateStrstring()
+	{
+		String dateformat = "10/09/2017";
+		String dateformatedit = "";
+		dateformatedit = dateformat.substring( 0, 3);
+		String datenewformat = "09 - 10 - 2017"
+	}
+	
+	public static void date()
+	{
+		
+	}
+	public static void multiplicationtable()
+	{
+		int count = 0;
+		for(int n = 0; count <= 0; n++)
+		{
+			System.out.println(n * r);
+		}
+		count ++;
+	}
 }
