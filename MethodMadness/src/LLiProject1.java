d4a7c24bfb99f8b7167ccc3ef30c36717e2acbf7
Lily <Lily@Kieying-Lily>
2017-10-09 22:59:22 -0700

    Almost thereee

--- MethodMadness/src/LLiProject1.java
+++ MethodMadness/src/LLiProject1.java
@@ -4,10 +4,27 @@
 
 public class LLiProject1 {
 	public static boolean isPalindrome(String word) {
+		String foward = " ", backward = " ";
 		char[] characters = word.toCharArray();
-	    for(int i = 0; i<= characters.length; i++) {
-	    	System.out.print(characters[i]); 
+		int index = characters.length - 1;
+	    for(int f = 0; f<= index; f++) {
+	    	foward+=characters[f];
+	    	//System.out.println(foward);
 	    }
-	    return true; 
+	    for(int b = index; b >= 0; b--) {
+	    	backward+=characters[b];
+	    	//System.out.println(backward);
+	    }
+	    return(foward==backward);   
+	}
+	
+	public static int sumUpTo(int integer) {
+		int num = integer - 1, sum = integer;
+		while(num != 0) {
+			sum += num;
+			num--;
+		}
+		//System.out.println(sum);
+		return sum;
 	}
 }
--- MethodMadness/src/Runner.java
+++ MethodMadness/src/Runner.java
@@ -1,4 +1,10 @@
-
+//Authors: Lily Li & Alyssa Ma
+//Title: Project 1: Method Madness
+//Date Created: October 5th 2017
+//Date Completed: 
 public class Runner {
-
+	public static void main (String [] args) {
+		LLiProject1.isPalindrome("noon");
+		LLiProject1.sumUpTo(4);
+	}
 }
