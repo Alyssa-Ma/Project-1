916906281687e896627cc8c491f619ccc2065e94
Alyssa <Alyssa@192.168.1.151>
2017-10-10 21:30:14 -0400

    i'm really dumb and i can't do this

--- MethodMadness/src/AMaProject1.java
+++ MethodMadness/src/AMaProject1.java
@@ -6,9 +6,9 @@ public class AMaProject1
 		String date = "mm/dd/yyyy";
 		String mmString = date.substring(0, 2);
 		String ddString = date.substring(3, 5);
-		String yyString = date.substring(6, 8);
+		String yyyyString = date.substring(6, 10);
 		{
-			return ddString + "-" + mmString + "-" + yyString;
+			return ddString + "-" + mmString + "-" + yyyyString;
 		}
 	}
 	public static void multiplicationtable(int r)
