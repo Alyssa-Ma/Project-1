5d6f92d646b8973dfc8f6a265451bdaae87cb38f
BT_1N3_30 <BT_1N3_30@BT_1N3_30.bktech.local>
2017-10-12 09:13:08 -0400

    lily yay

--- MethodMadness/src/LLiProject1.java
+++ MethodMadness/src/LLiProject1.java
@@ -1,21 +1,18 @@
 //Author: Lily Li
 //Date Created: 10/5/2017
-// Date Completed:
+// Date Completed: 10/12/2017
 
 public class LLiProject1 {
 	public static boolean isPalindrome(String word) {
-		String foward = " ", backward = " ";
+		String backward = "";
 		char[] characters = word.toCharArray();
 		int index = characters.length - 1;
-	    for(int f = 0; f<= index; f++) {
-	    	foward+=characters[f];
-	    	//System.out.println(foward);
+	
+	    for(int i = index; i >= 0; i--) {
+	    	backward+=characters[i];
 	    }
-	    for(int b = index; b >= 0; b--) {
-	    	backward+=characters[b];
-	    	//System.out.println(backward);
-	    }
-	    return(foward==backward);   
+	    	
+	    return(word.equals(backward));   
 	}
 	
 	public static int sumUpTo(int integer) {
@@ -24,7 +21,58 @@ public class LLiProject1 {
 			sum += num;
 			num--;
 		}
-		//System.out.println(sum);
 		return sum;
+	}	
+	
+	//Vigenere Cipher Encoder
+	public static String vigCipher(String message, String key) {
+		char[] keyCharacters = key.toCharArray(), msgCharacters = message.toCharArray();
+		int keyLength = keyCharacters.length, msgLength = msgCharacters.length, i = 0, num;
+		int keyNum[] = new int[keyLength];
+		String vigMsg = "";
+		
+		for(int x = 0; x <= msgLength - 1; x++) {
+			if(i > keyLength - 1) {//Enable pattern for key word to start over
+				i = 0;
+			}
+			
+	    	keyNum[i] = letterToNum(keyCharacters[i]);
+	    	
+	    	if(msgCharacters[x] == (char)32) {//this is for the spaces in the message
+	    		vigMsg += " ";	
+	    	}
+	    	else //find the num on the number chart 0-25
+	    	{
+		    	num = msgCharacters[x] - 'A' + keyNum[i]; 
+		    	
+		    	if(num >=26) {
+		    		num-=26;
+		    		vigMsg += numToLetter(num);
+		    		i++;
+		    	}
+		    	else {
+		    		vigMsg += numToLetter(num);
+		    		i++;
+		    		
+		    	}
+	    	}	
+		} 
+		
+		return vigMsg;
 	}
+	
+	private static int letterToNum(char letter) {
+		return(letter - 'A');
+	}
+	
+	private static char numToLetter(int num) {
+		char alphabet[] = new char[26];
+		
+		for(int i = 0; i <=25; i++) {
+			alphabet[i] = (char)(65+i); 
+		}
+		
+		return(alphabet[num]);
+	}
+	
 }
--- MethodMadness/src/Runner.java
+++ MethodMadness/src/Runner.java
@@ -1,15 +1,22 @@
 
+
+
+
 //Authors: Lily Li & Alyssa Ma
 //Title: Project 1: Method Madness
 //Date Created: October 5th 2017
 //Date Completed: 
 public class Runner {
 	public static void main (String [] args) {
-		LLiProject1.isPalindrome("noon");
-		LLiProject1.sumUpTo(4);
+		boolean palindromeResult = LLiProject1.isPalindrome("noon");
+		int sumUpToResult = LLiProject1.sumUpTo(3);
+		String vigMessage = LLiProject1.vigCipher("ATTACK AT DAWN", "LEMON");
+		
+		System.out.println(palindromeResult);
+		System.out.println(sumUpToResult);
+		System.out.println(vigMessage);
 		System.out.println(AMaProject1.dateStrstring("05/12/1965"));
 		AMaProject1.multiplicationtable(6, 8);
-		System.out.println(AMaProject1.cutOut("I. want. pizza."));
-		//cuts out first instance of a period
+		System.out.println(AMaProject1.cutOut());
 	}
 }
